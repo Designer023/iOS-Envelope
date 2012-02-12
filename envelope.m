046f2767d0f993e1c53c468b6afb3fef28b558f0
Carl Topham <carl@carl-topham.com>
2012-02-12 21:58:45 +0000

    Update envelope.m

--- envelope.m
+++ envelope.m
@@ -1,9 +1,8 @@
 //
 //  envelope.m
-//  truelove
 //
 //  Created by Carl Topham on 12/02/2012.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012. All rights reserved.
 //
 
 #import "envelope.h"
